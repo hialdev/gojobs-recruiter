19370d2b49aef192151f808bcbeb7bc87876d2c4
hialdev <al.developer12@gmail.com>
2023-12-07 17:07:11 +0700

    newpage and responsive

--- components/seeker/header.vue
+++ components/seeker/header.vue
@@ -15,7 +15,7 @@
                         <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 14 20"><path d="M12.133 10.632v-1.8A5.406 5.406 0 0 0 7.979 3.57.946.946 0 0 0 8 3.464V1.1a1 1 0 0 0-2 0v2.364a.946.946 0 0 0 .021.106 5.406 5.406 0 0 0-4.154 5.262v1.8C1.867 13.018 0 13.614 0 14.807 0 15.4 0 16 .538 16h12.924C14 16 14 15.4 14 14.807c0-1.193-1.867-1.789-1.867-4.175ZM3.823 17a3.453 3.453 0 0 0 6.354 0H3.823Z"/></svg>
                     </button>
                     <!-- Dropdown menu -->
-                    <div v-if="notifShow == true" class="absolute top-[2em] end-0 min-w-[20em] max-w-[30em] w-full overflow-hidden z-50 my-4 max-w-sm text-base list-none bg-white rounded divide-y divide-gray-100 shadow-lg">
+                    <div v-if="notifShow == true" class="absolute top-[2em] end-[-6em] md:end-0 min-w-[20em] max-w-[30em] w-full overflow-hidden z-50 my-4 max-w-sm text-base list-none bg-white rounded divide-y divide-gray-100 shadow-lg">
                         <div class="block py-2 px-4 text-base font-medium text-center text-gray-700 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                             Notifications
                         </div>
