4c2e40d74cc5b01c886457cf8d5ac9e996e23177
hialdev <al.developer12@gmail.com>
2024-01-19 18:03:24 +0700

    clear recurt --CUD

--- components/partial/header/profile.vue
+++ components/partial/header/profile.vue
@@ -5,7 +5,7 @@
             <span class="hidden md:block">Name Surname</span>
             <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 48 48"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="M36 18L24 30L12 18"/></svg>
         </div>
-        <div v-if="profileShow" class="absolute top-0 end-0 mt-[2.5em] z-10 min-w-[13em]">
+        <div v-if="profileShow" class="absolute top-0 end-0 mt-[2.5em] z-[999] min-w-[13em]">
             <div class="bg-white p-4 rounded-lg">
                 <div class="p-4 rounded-lg bg-emerald-50 mb-3">
                     <div class="flex items-center justify-center mb-2">
